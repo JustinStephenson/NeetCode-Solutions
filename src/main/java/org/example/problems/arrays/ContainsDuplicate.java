4c1e1bc62e336bc4fcf55d14649bc00e45bf0f3f
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2024-11-17 14:03:00 -0800

    initial commit

---
+++ src/main/java/org/example/problems/arrays/ContainsDuplicate.java
@@ -0,0 +1,11 @@
+package org.example.problems.arrays;
+
+/*
+    Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+ */
+
+public class ContainsDuplicate {
+    public boolean hasDuplicate(int[] nums) {
+        return true;
+    }
+}
