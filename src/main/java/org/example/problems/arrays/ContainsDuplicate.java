71ab6a8eb292fa85b0a8d8bb2541abd15ac505b2
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2024-11-18 16:42:19 -0800

    Finished ContainsDuplicate

--- src/main/java/org/example/problems/arrays/ContainsDuplicate.java
+++ src/main/java/org/example/problems/arrays/ContainsDuplicate.java
@@ -4,8 +4,16 @@ package org.example.problems.arrays;
     Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
  */
 
+import java.util.HashSet;
+
 public class ContainsDuplicate {
     public boolean hasDuplicate(int[] nums) {
-        return true;
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
