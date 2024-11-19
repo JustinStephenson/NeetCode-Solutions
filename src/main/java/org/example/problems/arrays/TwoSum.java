99c870c47512b117c3c71a93912ed0c9720c3a22
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2024-11-18 17:44:46 -0800

    Finished TwoSum

--- src/main/java/org/example/problems/arrays/TwoSum.java
+++ src/main/java/org/example/problems/arrays/TwoSum.java
@@ -1,4 +1,28 @@
 package org.example.problems.arrays;
 
+/*
+Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+Return the answer with the smaller index first.
+ */
+
+import java.util.HashMap;
+
 public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (map.containsKey(diff)) {
+                return new int[]{map.get(diff), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
 }
