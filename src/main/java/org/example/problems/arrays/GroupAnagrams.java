8623e7af71b949b2704979a60a620d203bc66f15
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2024-11-22 10:56:58 -0800

    Finished GroupAnagrams

--- src/main/java/org/example/problems/arrays/GroupAnagrams.java
+++ src/main/java/org/example/problems/arrays/GroupAnagrams.java
@@ -1,4 +1,25 @@
 package org.example.problems.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> resultMap = new HashMap<>();
+
+        for (String str : strs) {
+            int[] count = new int[26];
+
+            for (char c : str.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            resultMap.putIfAbsent(key, new ArrayList<>());
+            resultMap.get(key).add(str);
+        }
+
+        return new ArrayList<>(resultMap.values());
+    }
 }
