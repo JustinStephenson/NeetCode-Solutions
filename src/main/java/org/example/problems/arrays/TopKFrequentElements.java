cf6cee8de1d84908bb5617dd59c33df39e02ccf4
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2024-11-22 12:06:16 -0800

    Finished KTopFrequentElements

--- src/main/java/org/example/problems/arrays/TopKFrequentElements.java
+++ src/main/java/org/example/problems/arrays/TopKFrequentElements.java
@@ -1,4 +1,46 @@
 package org.example.problems.arrays;
 
+/*
+    Given an integer array nums and an integer k, return the k most frequent elements within the array.
+
+    The test cases are generated such that the answer is always unique.
+
+    You may return the output in any order.
+ */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TopKFrequentElements {
+    public int[] topKFrequent(int[] nums, int k) {
+        // Build buckets
+        List<Integer>[] buckets = new List[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            buckets[i] = new ArrayList<>();
+        }
+
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            buckets[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+        for (int i = buckets.length - 1; i > 0 && index < k; i--) {
+            List<Integer> bucket = buckets[i];
+            for (int n : bucket) {
+                result[index++] = n;
+                if (index == k) {
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
 }
