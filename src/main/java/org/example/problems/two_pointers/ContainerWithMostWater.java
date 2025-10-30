7061dd69204303ba677de0eddcdba1398d1147cf
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2025-10-29 19:30:19 -0700

    solved ContainerWithMostWater

--- src/main/java/org/example/problems/two_pointers/ContainerWithMostWater.java
+++ src/main/java/org/example/problems/two_pointers/ContainerWithMostWater.java
@@ -1,7 +1,27 @@
 package org.example.problems.two_pointers;
 
+/*
+    You are given an integer array 'heights' where 'heights[i]' represents the height of the ith bar.
+
+    You may choose any two bars to form a container. Return the maximum amount of water a container can store.
+ */
 public class ContainerWithMostWater {
     public int maxArea(int[] heights) {
-        return 0;
+        int leftPointer = 0;
+        int rightPointer = heights.length - 1;
+        int maxArea = 0;
+
+        while (leftPointer < rightPointer) {
+            int newMaxArea = (rightPointer - leftPointer) * Math.min(heights[leftPointer], heights[rightPointer]);
+            maxArea = Math.max(maxArea, newMaxArea);
+
+            if (heights[leftPointer] < heights[rightPointer]) {
+                leftPointer++;
+            } else {
+                rightPointer--;
+            }
+        }
+
+        return maxArea;
     }
 }
