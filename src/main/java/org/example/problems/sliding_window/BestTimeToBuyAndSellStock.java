f5eceb28ce671a388e44a7117ffaac83444abc00
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2025-11-02 13:46:50 -0800

    Completed BestTimeToBuyAndSellStock

--- src/main/java/org/example/problems/sliding_window/BestTimeToBuyAndSellStock.java
+++ src/main/java/org/example/problems/sliding_window/BestTimeToBuyAndSellStock.java
@@ -2,6 +2,18 @@ package org.example.problems.sliding_window;
 
 public class BestTimeToBuyAndSellStock {
     public int maxProfit(int[] prices) {
-        return 0;
+        int leftPointer = 0;
+        int rightPointer = 0;
+        int maxProfit = 0;
+
+        for (; rightPointer < prices.length; rightPointer++) {
+            maxProfit = Math.max(maxProfit, prices[rightPointer] - prices[leftPointer]);
+
+            if (prices[rightPointer] < prices[leftPointer]) {
+                leftPointer = rightPointer;
+            }
+        }
+
+        return maxProfit;
     }
 }
