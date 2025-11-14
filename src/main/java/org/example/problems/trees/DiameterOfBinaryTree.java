c2e032512f014d2e9b8eff41eaa11ae8a9edc2a2
Justin Stephenson <31748422+JustinStephenson@users.noreply.github.com>
2025-11-13 19:32:20 -0800

    Completed DiameterOfBinaryTree

--- src/main/java/org/example/problems/trees/DiameterOfBinaryTree.java
+++ src/main/java/org/example/problems/trees/DiameterOfBinaryTree.java
@@ -1,4 +1,35 @@
 package org.example.problems.trees;
 
+import org.example.data_structures.TreeNode;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/*
+    The diameter of a binary tree is defined as the length of the longest path between any two nodes within the tree.
+    The path does not necessarily have to pass through the root.
+
+    The length of a path between two nodes in a binary tree is the number of edges between the nodes.
+    Note that the path cannot include the same node twice.
+
+    Given the root of a binary tree root, return the diameter of the tree.
+ */
 public class DiameterOfBinaryTree {
+    public int diameterOfBinaryTree(TreeNode root) {
+        AtomicInteger maxHeight = new AtomicInteger(0);
+        maxHeightOfTree(root, maxHeight);
+        return maxHeight.get();
+    }
+
+    public int maxHeightOfTree(TreeNode root, AtomicInteger maxHeight) {
+        if (root == null) {
+            return 0;
+        }
+
+        int diameterLeftTree = maxHeightOfTree(root.left, maxHeight);
+        int diameterRightTree = maxHeightOfTree(root.right, maxHeight);
+
+        maxHeight.set(Math.max(maxHeight.get(), diameterLeftTree + diameterRightTree));
+
+        return Math.max(diameterLeftTree, diameterRightTree) + 1;
+    }
 }
---
+++ src/test/java/org/example/problems/trees/DiameterOfBinaryTreeTest.java
@@ -0,0 +1,37 @@
+package org.example.problems.trees;
+
+import org.example.data_structures.TreeNode;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiameterOfBinaryTreeTest {
+
+    @ParameterizedTest
+    @MethodSource("diameterOfBinaryTreeTestProvider")
+    void diameterOfBinaryTreeTest(TreeNode root, int expected) {
+        DiameterOfBinaryTree diameterOfBinaryTree = new DiameterOfBinaryTree();
+        int result = diameterOfBinaryTree.diameterOfBinaryTree(root);
+        assertEquals(expected, result);
+    }
+
+    static Stream<Arguments> diameterOfBinaryTreeTestProvider() {
+        return Stream.of(
+                // Input: [1,null,2,3,4,5] → Expected: 3
+                Arguments.of(
+                        TreeNode.of(1, null, 2, 3, 4, 5),
+                        3
+                ),
+
+                // Input: [1,2,3] → Expected: 2
+                Arguments.of(
+                        TreeNode.of(1, 2, 3),
+                        2
+                )
+        );
+    }
+}
